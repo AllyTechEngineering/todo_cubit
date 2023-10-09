26f644e02a96cc1ca39266ee0c362a736dc11a8f
Admin <btaylor@allytechllc.com>
2023-10-09 09:59:44 -0400

    Completed step 34 ToDo Model implementation

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -1,7 +1,30 @@
+import 'package:uuid/uuid.dart';
 
+enum Filter {
+  all,
+  active,
+  completed,
+}
+
+Uuid uuid = Uuid();
 
 class Todo {
   late final String id;
   late final String desc;
   late final bool completed;
-}
+  Todo({required this.id, required this.desc, required this.completed});
+
+  List<Object?> get props => [id, desc, completed];
+
+  @override
+  String toString() {
+    return 'Todo{id=$id, desc=$desc, completed=$completed}';
+  }
+
+  Todo copyWith({String? id, String? desc, bool? completed}) {
+    return Todo(
+        id: id ?? this.id,
+        desc: desc ?? this.desc,
+        completed: completed ?? this.completed);
+  }
+} //class Todo 
